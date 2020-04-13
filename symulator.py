23d2052fcb93cfaa002f5848ef8f8d044130153c
Jakub <jakubtolsciuk@gmail.com>
2020-04-13 17:13:20 +0200

    Added parity bit

--- symulator.py
+++ symulator.py
@@ -5,8 +5,21 @@ def binarySymmetricChannel(packet, probability):
     bsc = komm.BinarySymmetricChannel(probability)
     return bsc(packet)
 
-packet = [0, 1, 1, 1, 0, 0, 0, 0, 0, 1]
-x = binarySymmetricChannel(packet, 0.1)
+def addParityBit(packet):   #wzgledem "1"
+    counter = 0
+    for x in packet:
+        if x==1:
+            counter+=1
+    if counter%2 == 0:
+        packet.append(0)
+    else:
+        packet.append(1)
+    return packet
 
-print(x)
+#testy
+packet = [0, 1, 1, 1, 0, 0, 1, 0, 0, 1]
+y = addParityBit(packet)
+#x = binarySymmetricChannel(packet, 0.1)
+print(y)
+#print(x)
 
